22eb969286ed35d5912b26f666b17247147f774f
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2024-06-13 20:46:49 -0500

    last changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,140 +1,215 @@
 #include "Arduino.h"
-#include <IoTdeviceLib.h>       //Librería con funciones de sensor - actuador
-#include <IoTcomLib.h>          //Librería con funciones de comunicación del dispositivo
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>
+#include "HX711.h"    // incluye libreria HX711
+#include <ThingSpeak.h>
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "Servo.h"
 
-//* Servo Pin
-const int servoPin = D3; //https://elosciloscopio.com/tutorial-servomotor-arduino-esp8266-y-esp32/ https://techatronic.com/esp8266-with-servo-motor-control-servo-motor-with-esp8266/
+#define DT D6     // DT de HX711 a pin digital 2
+#define SCK D7      // SCK de HX711 a pin digital 3
+#define SERVOPIN D3
 
-//* HX711 circuit wiring
-const int DT = D6; //GPIO 12
-const int SCK = D7; //GPIO 13
-const float CALIBRATING_FACTOR = 734.f;
+#define MAX_STRINGS 10
+String strings[MAX_STRINGS];
+String horaactual;
 
-float peso;
-float pesoPromedio;
+HX711 celda;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-int numMedicion = 0;                                //Establece el número consecutivo de observacion hecha
-const unsigned long postingInterval = 25L * 1000L;  //? Establece cada cuanto se envia a ThingSpeak (25 s)
-unsigned long lastConnectionTime = 0;               //Para controlar el tiempo de generar nueva medición
-long lastUpdateTime = 0;                            //Momento de la última actualización
+void ConectarRed(char ssid[], char password[]);
 
-//* onMessageArrived
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+void ConectarRed(char ssid[], char password[])
+{
+  Serial.println("Conectandose a la WIFI!");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
   }
-  Serial.println();
 
-  //TODO Deserializar el JSON https://arduinojson.org/
-
-  // Switch on the LED if an 0 was received as first character
-  // if ((char)payload[0] == '0') {
-  //   digitalWrite(bombillopin, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  //   // but actually the LED is on; this is because
-  //   // it is acive low on the ESP-01)
-  // } else {
-  //   digitalWrite(bombillopin, HIGH);  // Turn the LED off by making the voltage HIGH
-  // }
-
+  Serial.println("");
+  Serial.println("WiFi conectada");
+  Serial.println(WiFi.localIP());
 }
 
-//metodo cliente para controlar los eventos R1 y R2
-void setup()
+//* MQTT
+static unsigned long channelID = 2575111;
+static char* writeAPIKey = (char*)"979JJW97OBJG9ZY9"; 
+const char* mqttAddress = "mqtt3.thingspeak.com";
+static int port = 1883;
+static char* clientId = (char*)"DCwSPAozOAo0PBIYKiEsEjs";
+static char* username = (char*)"DCwSPAozOAo0PBIYKiEsEjs";
+static char* password = (char*)"VDzLkls5S1K8FThTVdHTIHsv";
+String PesoMax = "100";
+float peso = 0;
+Servo myservo;
+unsigned long lastWeightReadTime = 0;
+const unsigned long weightReadInterval = 2000;
+const unsigned long postingInterval = 20L * 1000L;
+long lastUpdateTime = 0;   
+
+void InicializarThingSpeak()
 {
-  //*---------------- Abrir el puerto de lectura en el PC para mensajes -----------------
-  Serial.begin(115200);
-
-  //*---------------- Conectarse a red WiFi -----------------
-  ConectarRed("ssid","password");
-  //-----Comando para Conectarse y configurar desde el Celular--------
-  // Creamos una instancia de la clase WiFiManager
-  // WiFiManager wifiManager;
-
-  // Descomentar para resetear configuración - Hacer el ejercicio con el celular
-  // todas las veces.
-  //wifiManager.resetSettings();
-
-  // Creamos AP y portal para configurar desde el Celular
-  // wifiManager.autoConnect("ESP8266Temp");
- 
-  // Serial.println("!Ya estás conectado¡");
-  //----------- Fin de conexión ESP8266 -----------------------------
-
-  //Establecer los modos de los puertos
-  //*---------------- Servomotor -----------------
-  miServo.attach(servoPin);
-  // miServo.write(0);
-  //*------------------- Hx711 ---------------------
-  celda.begin(DT, SCK);
-  celda.set_scale(CALIBRATING_FACTOR);
-  celda.tare();
-  //*-------------- Inicializar aqui thingspeak --------------
-  InicializarThingSpeak(); //? Para escribir
-  //*-------------- Inicializar servidor MQTT --------------
-  // Configuración del cliente MQTT: https://pubsubclient.knolleary.net/api
-  client.setServer(mqttAddress, port); //? Para leer
-  client.setCallback(callback);
-  // reconnect();
-  //*-------------- Inicializar NTPClient --------------
-  // https://randomnerdtutorials.com/esp8266-nodemcu-date-time-ntp-client-server-arduino/
-  timeClient.begin();
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
-  // GMT 5 = 18000
-  timeClient.setTimeOffset(18000);
+  //************ Conectar Cliente ThinkSpeak *******
+  ThingSpeak.begin( espClient );
+  //******** Fin Conectar Cliente ThingSpeak *******
 }
 
-void loop()                    
-{
-  //* Conectarse al servidor MQTT
-  if (!client.connected()) {
-    reconnect();
+// Contador de strings almacenados
+int numStrings = 0;
+
+// Función para verificar si un string está presente en el array
+bool stringPresente(String str) {
+  for (int i = 0; i < numStrings; i++) {
+    if (strings[i] == str) {
+      return true;
+    }
   }
-  client.loop();
-  
+  return false;
+}
+
+// Función para agregar un string al array si no está presente
+bool agregarString(String str) {
+  if (!stringPresente(str)) {
+    if (numStrings < MAX_STRINGS) {
+      strings[numStrings] = str;
+      numStrings++;
+      return true;
+    } else {
+      Serial.println("Error: El array está lleno.");
+      return false;
+    }
+  } else {
+    Serial.println("Error: String repetido no permitido.");
+    return false;
+  }
+}
+
+// Función para eliminar un string del array si está presente
+bool eliminarString(String str) {
+  for (int i = 0; i < numStrings; i++) {
+    if (strings[i] == str) {
+      for (int j = i; j < numStrings - 1; j++) {
+        strings[j] = strings[j + 1];
+      }
+      numStrings--; 
+      return true;
+    }
+  }
+  Serial.println("Error: String no encontrado.");
+  return false;
+}
+
+void imprimirStrings() {
+  Serial.println("Contenido del array:");
+  for (int i = 0; i < numStrings; i++) {
+    Serial.println(strings[i]);
+  }
+}
+
+int EnviarPeso(float data ){
+  int  writeSuccess = ThingSpeak.writeField( channelID, 1, data, "979JJW97OBJG9ZY9" ); // Write the data to the channel
+  if ( writeSuccess ){
+    Serial.println( String(data) + " Enviando a ThingSpeak." );
+  }
+    
+  return writeSuccess;
+}
+
+void LecturaPeso(){
+  String lectura = ThingSpeak.readStringField(2575111,1,"CT2DV6AUJSN2K65A");
+}
+
+void LecturaPesoMax(){
+  String lectura = ThingSpeak.readStringField(2575111,2,"CT2DV6AUJSN2K65A");
+  if(lectura != PesoMax){
+    PesoMax = lectura;
+    Serial.println( "cambio el Peso Max: " + PesoMax );
+  }
+}
+
+void agregarHorario(){
+  String horario = ThingSpeak.readStringField(2575111,5,"CT2DV6AUJSN2K65A");
+  Serial.println( "horario: " + horario );
+  if(agregarString(horario)){
+    Serial.println("horario " + horario + " agregado.");
+  }else{
+    Serial.println("horario " + horario + " no agregado.");
+  }
+}
+
+void eliminarHorario(){
+  String horario = ThingSpeak.readStringField(2575111,6,"CT2DV6AUJSN2K65A");
+  if(eliminarString(horario)){
+    Serial.println("horario " + horario + " eliminado.");
+  }
+}
+
+void Dispensar(){
   //* Obtener la hora actual
   timeClient.update();
-  Serial.print("=========== NTPClient ===========");
   int currentHour = timeClient.getHours();
-  Serial.print("Hour: ");
-  Serial.println(currentHour);  
-
   int currentMinute = timeClient.getMinutes();
-  Serial.print("Minutes: ");
-  Serial.println(currentMinute); 
-
-  delay(2000);
-
-  // Solamente actualiza si el tiempo de publicación es excedido
-  /*if (millis() - lastUpdateTime >=  postingInterval) {
-    lastUpdateTime = millis();
-
-    //* Leer sensor de peso
-    peso = LeerPeso();
-    pesoPromedio = LeerPromedioPeso(5);
-
-    //* Imprimir peso leído 
-    Serial.print("=========== Medición No.: ");
-    Serial.print(numMedicion++);
-    Serial.println(" ============");
+  horaactual = String(currentHour) + ":" + String(currentMinute); 
+  Serial.println( "hora actual > " + horaactual );
+  
+  if(stringPresente(horaactual)){
+    while(peso < PesoMax.toFloat()) {
+      //* Leer sensor de peso
+      celda.power_up();
+      peso = celda.get_units(10);
+      Serial.print("Valor (gramos): ");    
+      Serial.println(peso);
+      celda.power_down();                
+      for(int angle = 0; angle < 180; angle += 1)
+      {                          
+        myservo.write(angle);
+        delay(15);                       
+      } 
+      delay(1000);
       
-    ImprimirValorSensor(peso, "Peso en el plato", "Kg");
-    ImprimirValorSensor(pesoPromedio, "Peso(promedio) en el plato", "Kg");
+      for(int angle = 180; angle>=0; angle-=15)
+      {                                
+        myservo.write(angle);
+        delay(5);                       
+      } 
+    }
+    
+  }
+}
 
-    Serial.println("========================================");
+void setup() {
+  Serial.begin(115200);   // inicializa monitor serie a 9600 baudios
+  myservo.attach(SERVOPIN);
+  myservo.write(0);
+  celda.begin(DT, SCK);  
 
-    DispensarAlimento();
+  celda.set_scale(734.f);
+  celda.tare();     
+  ConectarRed("ssid", "pasword");
+  InicializarThingSpeak();
+  timeClient.begin();
+  timeClient.setTimeOffset(-18000);
+}
 
-    //* Enviar peso leído a ThinkSpeak  
-    EnviarThingSpeakDatos(dataFieldOne, peso);
-  }*/
+void loop() {
+  
+  if (millis() - lastWeightReadTime >= weightReadInterval) {
+    lastWeightReadTime = millis();
+
+    Dispensar();
+  }
+
+  if (millis() - lastUpdateTime >=  postingInterval) {
+    lastUpdateTime = millis();
+    EnviarPeso(peso);
+    LecturaPeso();
+    LecturaPesoMax();
+    agregarHorario();
+    imprimirStrings();
+  }
 }
